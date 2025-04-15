ccaabfe909197431bbe8342121b2f98ecf3a546a
N08I40K <nhomcencko1@gmail.com>
2025-04-15 18:44:03 +0400

    Асинхронный вариант MutexScope.

--- src/utility/mutex.rs
+++ src/utility/mutex.rs
@@ -15,7 +15,29 @@ where
     fn scope(&self, f: ScopeFn) -> ScopeFnOutput {
         let mut lock = self.lock().unwrap();
         let inner = lock.deref_mut();
-        
+
         f(inner)
+    }
+}
+
+pub trait MutexScopeAsync<T> {
+    async fn async_scope<'a, F, FnFut, FnOut>(&'a self, f: F) -> FnOut
+    where
+        FnFut: Future<Output = FnOut>,
+        F: FnOnce(&'a mut T) -> FnFut,
+        T: 'a;
+}
+
+impl<T> MutexScopeAsync<T> for Mutex<T> {
+    async fn async_scope<'a, F, FnFut, FnOut>(&'a self, f: F) -> FnOut
+    where
+        FnFut: Future<Output = FnOut>,
+        F: FnOnce(&'a mut T) -> FnFut,
+        T: 'a,
+    {
+        let mut guard = self.lock().unwrap();
+
+        let ptr: &'a mut T = unsafe { &mut *(guard.deref_mut() as *mut _) };
+        f(ptr).await
     }
 }
